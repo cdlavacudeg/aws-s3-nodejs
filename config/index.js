c9ef85a5f21f384ff610c80ca7972abc5decf0f7
cdlavacudeg <cristianlav10@gmail.com>
2023-06-18 11:16:27 -0500

    [Add] Enviroment variables

---
+++ config/index.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const config = {
+  env: process.env.NODE_ENV,
+  api: {
+    port: process.env.API_PORT,
+  },
+  s3: {
+    bucket: process.env.S3_BUCKET,
+    region: process.env.S3_REGION,
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
+
+module.exports = config;
